b07d77d2d18d911158bd41989ff3c8ad62ee1ef4
brent113 <brent113@users.noreply.github.com>
2014-03-21 13:49:50 -0700

    Update application.js

--- js/application.js
+++ js/application.js
@@ -1,5 +1,5 @@
-animationDelay = 100;
-minSearchTime = 100;
+animationDelay = 5;
+minSearchTime = 10;
 
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
